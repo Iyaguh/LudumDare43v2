1c6f125c22220108f711500b83aec7e9e90831fc
BabushkaEffect <akishchinskiy@gmail.com>
2018-12-02 15:00:40 +0300

    переменные + сцена

---
+++ Ld43v2/Assets/GameHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+
+    public int cycle = 0;
+    public int money = 20;
+    public float fear = 5;
+    public float population = 20;
+    public float god1 = 20;
+    public float god2 = 20;
+    public float god3 = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
